b7317ea9cc6ac84581731a14bd4e29ca77acf779
Jian Zhou <i@janzhou.org>
2016-04-25 15:43:56 -0400

    fix sync in partital write

--- src/main/scala/org/janzhou/ssd_blkdev/main.scala
+++ src/main/scala/org/janzhou/ssd_blkdev/main.scala
@@ -11,9 +11,7 @@ object main {
     var _heartbeat = 0
 
     def heartbeat = {
-      this.synchronized {
-        _heartbeat = 0
-      }
+      _heartbeat = 0
     }
 
     var _stop = false
@@ -25,15 +23,15 @@ object main {
       while ( !_stop ) {
         this.synchronized {
           _heartbeat += 1
-        }
-        if ( _heartbeat >= 1024 ) {
-          this.synchronized {
+          if ( _heartbeat >= 1024 ) {
             println("background gc")
             if (!ftl.gc) {
               _heartbeat = 0
             }
           }
-        } else {
+        }
+
+        if ( _heartbeat < 1024 ) {
           Thread.sleep( 1000 )
         }
       }
@@ -73,11 +71,6 @@ object main {
 
     val time = new SimpleDateFormat("HH:mm:ss")
 
-    var last_dir = 0
-    var last_lpn = 0
-    var last_ppn = ftl.read(last_lpn)
-    //println(time.format(Calendar.getInstance().getTime()) + " R " + last_lpn + " " + last_ppn)
-
     val gc_thread = new GCThread(ftl)
     gc_thread.start()
 
@@ -93,35 +86,34 @@ object main {
       val request_map = libc.run().calloc(req_size.getInt(0), 88) //sizeof(*request_map)
       libc.run.ioctl(fd, 0x80087802, request_map) //SSD_BLKDEV_GET_LBN
 
-      gc_thread.heartbeat
+      gc_thread.synchronized {
+        for( i <- 0 to req_size.getInt(0) - 1 ) {
+          val offset = i * 88
+          val dir = request_map.getInt(offset + 0)
+          val num_sectors = request_map.getInt(offset + 4)
+          val start_lba = request_map.getInt(offset + 8)
+          val psn_offset = offset + 16
 
-      for( i <- 0 to req_size.getInt(0) - 1 ) {
-        val offset = i * 88
-        val dir = request_map.getInt(offset + 0)
-        val num_sectors = request_map.getInt(offset + 4)
-        val start_lba = request_map.getInt(offset + 8)
-        val psn_offset = offset + 16
+          var last_lpn = -1
+          var last_ppn = -1
 
-        for( i <- 0 to num_sectors - 1 ) {
-          val sector = start_lba + i
+          for( i <- 0 to num_sectors - 1 ) {
+            val sector = start_lba + i
 
-          val lpn = sector / device.SectorsPerPage
-          val sector_offset = sector % device.SectorsPerPage
+            val lpn = sector / device.SectorsPerPage
+            val sector_offset = sector % device.SectorsPerPage
 
-          //println("dir " + dir + " sector " + sector + " lpn " + lpn + " offset " + sector_offset)
+            //println("dir " + dir + " sector " + sector + " lpn " + lpn + " offset " + sector_offset)
 
-          val ppn = if(lpn == last_lpn && dir == last_dir) {
-            last_ppn
-          } else {
-              if( dir == 0 ) {
-              val ppn = gc_thread.synchronized {
-                ftl.read(lpn)
-              }
-              //println(time.format(Calendar.getInstance().getTime()) + " R " + lpn + " " + ppn)
-              ppn
+            val ppn = if(lpn == last_lpn) {
+              last_ppn
             } else {
-              val ppn = gc_thread.synchronized {
-                if ( sector_offset != 0 || num_sectors - i < device.SectorsPerPage ) {
+              if( dir == 0 ) {
+                val ppn = ftl.read(lpn)
+                //println(time.format(Calendar.getInstance().getTime()) + " R " + lpn + " " + ppn)
+                ppn
+              } else {
+                val ppn = if ( sector_offset != 0 || num_sectors - i < device.SectorsPerPage ) {
                   //println("partial write lpn " + lpn + " sector " + sector + " offset " + sector_offset + " start_lba " + start_lba + " num_sectors " + num_sectors)
                   val old_ppn = ftl.read(lpn)
                   val new_ppn = ftl.write(lpn)
@@ -130,18 +122,18 @@ object main {
                 } else {
                   ftl.write(lpn)
                 }
+                //println(time.format(Calendar.getInstance().getTime()) + " W " + lpn + " " + ppn)
+                ppn
               }
-              //println(time.format(Calendar.getInstance().getTime()) + " W " + lpn + " " + ppn)
-              ppn
             }
+
+            last_lpn = lpn
+            last_ppn = ppn
+
+            request_map.setLong(psn_offset + i * 8, ppn * device.SectorsPerPage + sector_offset)
           }
-
-          last_lpn = lpn
-          last_ppn = ppn
-          last_dir = dir
-
-          request_map.setLong(psn_offset + i * 8, ppn * device.SectorsPerPage + sector_offset)
         }
+        gc_thread.heartbeat
       }
 
       libc.run.ioctl(fd, 0x40087803, request_map) //SSD_BLKDEV_GET_LBN
