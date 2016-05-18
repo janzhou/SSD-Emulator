a80f9fa17a3ab7020c64b425d83fbdcf77c4e511
Jian Zhou <i@janzhou.org>
2016-05-18 13:41:19 -0400

    update lshminer

--- src/main/scala/org/janzhou/cminer/LSHMiner.scala
+++ src/main/scala/org/janzhou/cminer/LSHMiner.scala
@@ -12,27 +12,20 @@ class LSHMiner (
 ) extends CMiner (minSupport, splitSize, depth) {
   private val lsh = new LSHSuperBit(stages, buckets, splitSize)
 
+  private def lshGroup(splits:ArrayBuffer[ArrayBuffer[Int]], minSupport:Int)
+  :ArrayBuffer[ArrayBuffer[ArrayBuffer[Int]]] = {
+    splits.map( seq =>
+      (lsh.hash(seq.toArray)(stages - 1), seq)
+    ).groupBy( _._1 ).filter(_._2.length >= minSupport)
+    .map(_._2.map(_._2)).to[ArrayBuffer]
+  }
+
   override def mine(seq:ArrayBuffer[Int]):ArrayBuffer[ArrayBuffer[Int]] = {
     val groups = seq.grouped(splitSize).to[ArrayBuffer]
 
-    val lsh_buckets = groups.map( seq =>
-      (lsh.hash(seq.toArray)(stages - 1), seq)
-    ).groupBy( _._1 ) // Map( group -> ( group, ArrayBuffer[ArrayBuffer[Int]] ) )
-
-    /*
-    val keys = lsh_buckets.mapValues(_.size).toArrayBuffer // ( key, count )
-
-    val min = groups.length * minSupport
-
-    val hot = keys.filter( _._2 >= min ).map(_._1)
-
-    val hot_buckets = hot.map(
-      lsh_buckets(_).map(_._2)
-    )
-
-    val freqInBucket = hot_buckets.map( split => mineSplit(split) )
-    */
-    val freqInBucket = lsh_buckets.map(_._2).map( splits => mineSplits(splits.map(_._2)) )
+    val freqInBucket = lshGroup(groups, minSupport).map( splits => {
+      mineSplits(splits)
+    })
 
     freqInBucket.fold(ArrayBuffer[ArrayBuffer[Int]]())( _ ++= _ ).distinct
   }
