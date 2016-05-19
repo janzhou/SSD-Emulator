db90ca0469b6c25eb206d72b80008957f402069c
Jian Zhou <i@janzhou.org>
2016-05-19 11:36:48 -0400

    added CMinerTestFile

---
+++ src/test/scala/org/janzhou/CMinerTestFile.scala
@@ -0,0 +1,17 @@
+package org.janzhou.test
+
+import org.janzhou.cminer._
+import scala.collection.mutable.ArrayBuffer
+
+import java.io._
+
+object CMinerTestFile {
+  def main (args: Array[String]) {
+    val oos = new ObjectInputStream(new FileInputStream(args(0)))
+    val seq = oos.readObject().asInstanceOf[ArrayBuffer[Int]]
+    oos.close
+
+    val miner = new LSHMiner(2, 64, 16, seq.length)
+    miner.mine(seq).map(println)
+  }
+}
